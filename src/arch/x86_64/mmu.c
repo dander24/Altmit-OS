5ffd409eb0b88f8ef6ec9346a18aa785e3b2b3a9
dander24 <dander24@calpoly.edu>
2017-05-14 16:12:02 -0700

    workingish stack allocator

--- src/arch/x86_64/mmu.c
+++ src/arch/x86_64/mmu.c
@@ -4,7 +4,6 @@
 #include "bio.h"
 #include <stdint-gcc.h>
 
-
 //tag types
 #define MEMORY_MAP 6
 #define ELF_SYMBOLS 9
@@ -14,10 +13,12 @@
 
 #define MAX_BLOCKS 3
 #define FOURK 4096
+#define TWOMB FOURK * 500
 #define PCOUNT 512
 
 static uint32_t align8(uint32_t num);
 static uint64_t align4k(uint64_t num);
+void* walk_page_table(uint64_t addr);
 
 typedef struct free_pool{
 	//void* page;
@@ -81,9 +82,133 @@ typedef struct elfheader{
 	elf_section section_header[0];
 } __attribute__((packed)) elf_header;
 
+typedef struct virtualAddr{
+uint64_t pys_offset:12;
+uint64_t pt1_offset:9;
+uint64_t pt2_offset:9;
+uint64_t pt3_offset:9;
+uint64_t pt4_offset:9;
+uint64_t sign_ext:8;
+}__attribute__((packed)) virt_addr;
+
+typedef struct memallocinfo{
+uint16_t p4;
+uint16_t p3;
+uint16_t p2;
+uint16_t p1;
+} mem_alloc_info;
+
 static mem_block avail_mem[MAX_BLOCKS];
 static free_pool* free_pages;
 static PT4* page_table; //same as value in CR3, so we don't have to asm every time
+static void* max_addr;
+static mem_alloc_info cur_stack, cur_heap;
+
+void flush_tlb()
+{
+	uint64_t temp;
+	asm volatile ("movq %%cr3, %0": "=r"(temp):);
+	asm volatile ("movq %0, %%cr3": : "r"(temp));
+}
+
+
+void* alloc_stack()
+{
+	CLI;
+	uint64_t cur = page_table;
+	PT4  *pt4p;
+	PT3  *pt3p;
+	PT2  *pt2p;
+	PT1  *pt1p;
+	
+	virt_addr ret;
+	void* retval;
+	
+	
+	flush_tlb();
+	pt4p = (PT4*) cur;
+	if(pt4p[cur_stack.p4].A == 0)
+	{
+		pt4p[cur_stack.p4].BASE = ((uint64_t)alloc_pf() >> 12);
+		pt4p[cur_stack.p4].A = 1;
+		pt4p[cur_stack.p4].P = 1;
+		pt4p[cur_stack.p4].US = 0;
+		pt4p[cur_stack.p4].RW = 1;
+	}
+	
+	pt3p = (PT3*) pt4p[cur_stack.p4].BASE;
+	if(pt3p[cur_stack.p3].A == 0)
+	{
+		pt3p[cur_stack.p3].BASE = ((uint64_t)alloc_pf() >> 12);
+		pt3p[cur_stack.p3].A = 1;
+		pt3p[cur_stack.p3].P = 1;
+		pt3p[cur_stack.p3].US = 0;
+		pt3p[cur_stack.p3].RW = 1;
+	}
+
+	pt2p = (PT2*) pt3p[cur_stack.p3].BASE;
+	if(pt2p[cur_stack.p2].A == 0)
+	{
+		pt2p[cur_stack.p2].BASE = ((uint64_t)alloc_pf() >> 12);
+		pt2p[cur_stack.p2].A = 1;
+		pt2p[cur_stack.p2].P = 1;
+		pt2p[cur_stack.p2].US = 0;
+		pt2p[cur_stack.p2].RW = 1;
+	}
+	pt1p = (PT1*) pt2p[cur_stack.p2].BASE;
+	for(int i = 0; i < PCOUNT; i++)
+	{
+		pt1p[cur_stack.p1].A = 0; //needs to be faulted in
+		pt1p[cur_stack.p1].P = 1;
+		pt1p[cur_stack.p1].US = 0;
+		pt1p[cur_stack.p1].RW = 1;
+		cur_stack.p1++; //incr stack state
+	}
+	
+	if(cur_stack.p1 == PCOUNT)
+	{
+		cur_stack.p2++;
+		cur_stack.p1 = 0;
+		if(cur_stack.p2 == PCOUNT)
+		{
+			cur_stack.p3++;
+			cur_stack.p2 = 0;
+			if(cur_stack.p3 == PCOUNT)
+			{
+				cur_stack.p4++;
+				cur_stack.p3 = 0;
+			}
+		}
+	}
+
+	printk("%d %d %d %d\n", cur_stack.p1, cur_stack.p2, cur_stack.p3, cur_stack.p4);
+	
+	//return top of stack
+	ret.pys_offset = 0;
+	ret.pt1_offset = cur_stack.p1;
+	ret.pt2_offset = cur_stack.p2;
+	ret.pt3_offset = cur_stack.p3;
+	ret.pt4_offset = cur_stack.p4;
+	retval = *((uint64_t*) &ret);
+	return retval;
+}
+
+void* walk_page_table(uint64_t addr)
+{
+	virt_addr vadd;
+	printk("addr %p \n", addr);
+	vadd =  *((virt_addr*) &addr);
+	printk("vaddr %p \n", vadd);
+	uint64_t cur = page_table;
+	
+	cur = ((PT4*)cur)[vadd.pt4_offset].BASE << 12;
+	cur = ((PT3*)cur)[vadd.pt3_offset].BASE << 12;
+	cur = ((PT2*)cur)[vadd.pt2_offset].BASE << 12;
+	cur = ((PT1*)cur)[vadd.pt1_offset].BASE << 12;
+	cur = cur + vadd.pys_offset;
+	printk("res %p \n", cur);
+	return cur;
+}
 
 void init_page_table()
 {
@@ -97,25 +222,33 @@ void init_page_table()
 
 	//address used for identity map
 	uint64_t cur_addr = 0;
-
 	
+	//prep heap 
+	cur_stack.p4 = 2;
+	cur_stack.p3 = 0;
+	cur_stack.p2 = 0;
+	cur_stack.p1 = 0;
+	
+
 	//set cr3 and pointer to level four of page table
-	cr3r.base = page_table;
+	cr3r.base = (uint64_t)page_table >> 12;
+	cr3r.PWT = 1;
+	cr3r.PCD = 1;
 	pt4p = page_table;
 
-	printk("%p %p\n", cr3r, page_table);
 	//make the identity map in slot one of lvl 4
 	
 	//create pointer to level 3 table
-	pt4p[0].BASE = (PT3*) alloc_pf();
+	pt4p[0].BASE = (PT3*) ((uint64_t)alloc_pf() >> 12);
 	//point to level 3 table (only need one for identity map	
-	pt3p = pt4p[0].BASE;
+	pt3p = pt4p[0].BASE << 12;
+	printk("%p %p\n", pt3p, page_table);
 
 	//configure rest of current entry
 	pt4p[0].RW = 1;
 	pt4p[0].P = 1;
 	pt4p[0].US = 0; 
-	
+
 	//loop through every entry of level 3
 	for(int i = 0;  i < PCOUNT; i++)
 	{
@@ -124,7 +257,6 @@ void init_page_table()
 		pt3p[i].US = 0; 
 		pt3p[i].BASE = (PT2*) ((uint64_t)alloc_pf() >> 12);
 		pt2p = pt3p[i].BASE << 12;
-
 		//loop through every entry of level 2 (from cur level 3)
 		for(int j = 0; j < PCOUNT; j++)
 		{
@@ -140,12 +272,21 @@ void init_page_table()
 				pt1p[k].P = 1;
 				pt1p[k].US = 0; 
 				pt1p[k].BASE = cur_addr++;
+				if((cur_addr << 12) > max_addr)
+				{
+					int q = 1;
+					asm volatile ("movq %0, %%cr3": : "r"(cr3r));
+					flush_tlb();
+					printk("%p \n", alloc_stack());
+					flush_tlb();
+					printk("%p \n", alloc_stack());
+					return;
+				}
 			}
 
 		}
 	
 	}
-	//asm("movq cr3, %0": "=a"(cr3r): );
 	printk("%p %p\n", cr3r, page_table);
 }
 
@@ -208,8 +349,11 @@ void free_pf(void* freed)
 static void parse_memtag(void* ELF)
 {
 	mem_map* map;
-	mem_info info;
+	mem_info info;	
 	uint32_t entry_size, total_size, entry_count, cur_block = 0;
+
+	//track end of memory space
+	max_addr = NULL;
 
 	map = (mem_map*) ELF;
 	entry_size = map->ent_size;
@@ -228,11 +372,13 @@ static void parse_memtag(void* ELF)
 			if(avail_mem[cur_block].head == 0)
 				avail_mem[cur_block].head += FOURK;
 			avail_mem[cur_block].tail = info.start_addr + info.length;
-		printk("%d::ADDR %qx LEN %qx\n", i, avail_mem[cur_block].head, avail_mem[cur_block].tail-avail_mem[cur_block].head);	
+		//printk("%d::ADDR %qx LEN %qx\n", i, avail_mem[cur_block].head, avail_mem[cur_block].tail-avail_mem[cur_block].head);
 			cur_block++;
 		}
-		
+		if((info.start_addr + info.length)> max_addr)
+			 max_addr = info.start_addr + info.length;
 	}
+	printk("MAX: %p \n", max_addr);
 }
 
 static void parse_elfsym(void* ELF)
